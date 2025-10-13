921fff47b05ea73ccfd2ee9bdce3dade63a1801c
Oluwafemimicheal <oluwafemicu@gmail.com>
2025-10-13 22:13:45 +0100

    initial commit

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,13 @@
 const loading = document.getElementById("loading");
+const youtubeFullscreen = document.getElementById("youtube-fullscreen");
+
+setTimeout(() => {
+  loading.innerHTML =
+    "<h1 class='error-message'>Please check your network or <br> refresh this page!</h1>";
+}, 10000);
 
 window.addEventListener("load", () => {
   loading.style.display = "none";
 });
 
-setTimeout(() => {
-  loading.innerHTML =
-    "<h1 class='error-message'>Please check your network or <br> refresh this page!</h1>";
-}, 2000);
+//youtubeFullscreen.requestFullscreen();
